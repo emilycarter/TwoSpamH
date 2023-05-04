7feaae82980e8c07eaf1b462a5d7adfceddbbdd0
emilycarter <emilyjwcarter@gmail.com>
2023-05-04 10:12:20 -0400

    Update TwoSpamH.R

--- R/TwoSpamH.R
+++ R/TwoSpamH.R
@@ -97,7 +97,7 @@ TwoSpamH = function(data,
   if(is.numeric(variable)){
     variable = names(data)[variable]
   }
-  ex.usage.imp = data %>%  dplyr::ungroup() %>%
+  ex.usage.imp = data %>%
     dplyr::select(all_of(c(var.vec, variable, step2.var))) %>%
     mutate_if(is.numeric, imp.method)
 
