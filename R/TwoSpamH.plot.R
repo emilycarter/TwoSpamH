c4228080666b2ad5e920711e6c79d8bb5cecbaef
Emily Carter <emc4014@med.cornell.edu>
2023-10-05 10:36:48 -0400

    updated plot

--- R/TwoSpamH.plot.R
+++ R/TwoSpamH.plot.R
@@ -4,8 +4,6 @@
 #'
 #' @param TwoSpamH.output The output from the function TwoSpamH when the \code{plot.data} is set to TRUE
 #' @param to.plot A length 2 vector that indicates which principle components are to be plotted
-#' @param xlab The label for x axis
-#' @param ylab The label for y axis
 #' @param title The plot title
 #' @param variable The variable that is being plotted
 #'
@@ -25,25 +23,29 @@
 #' @export
 
 TwoSpamH.plot = function(TwoSpamH.output,
-                         to.plot = c(1,2),
-                         xlab = "PC 1",
-                         ylab = "PC 2",
-                         title = "Validity Distribition in the Constructed Feature Space",
-                         variable = "Filtered Variable"){
+                              to.plot = c(1,2),
+                              title = "Validity Distribition in the Constructed Feature Space",
+                              variable = "Filtered Variable"){
 
   df = TwoSpamH.output$df
   threshes.low = TwoSpamH.output$thresholds[[1]][to.plot]
   threshes.high = TwoSpamH.output$thresholds[[2]][to.plot]
   plot.vec = paste("pc", to.plot, sep = ".")
   title.ex = TwoSpamH.output$subj
+  df$x = df %>% pull(plot.vec[1])
+  df$y = df %>% pull(plot.vec[2])
 
-  ggplot(data = df, aes(x = pc.1, y = pc.2)) +
-    geom_point(aes(col = filter.new, size = filtered.var), alpha = 0.6) +
+  ggplot() +
+    geom_point(data = df, aes(x = x, y = y, col = filter.new, size = filtered.var), alpha = 0.4) +
     annotate("rect",xmin=-Inf,xmax=threshes.low[[1]],ymin=-Inf, ymax=threshes.low[[2]], fill = "red",alpha = 0.2) +
     annotate("rect",xmin=threshes.high[[1]],xmax=Inf,ymin=threshes.high[[2]], ymax=Inf, fill = "#00BFC4",alpha = 0.2) +
-    labs(title = title, x = xlab,
-         y = ylab) +
-    scale_color_discrete(name = "", labels = c("Valid", "Invalid")) +
+    labs(title = title, x = paste("PC",to.plot[1]),
+         y = paste("PC",to.plot[2])) +
+    scale_color_manual(name = "",
+                       values = c("filtered" = "red",
+                                  "not filtered" = "#00BFC4"),
+                       labels = c("filtered" = "Invalid",
+                                  "not filtered" = "Valid")) +
     scale_size_continuous(name = variable) +
     theme_bw()
 
